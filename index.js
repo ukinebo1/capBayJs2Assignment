f05e700eaf835b9b2fc83f1e563f33a9bb7d5359
ukinebo1 <112960629+ukinebo1@users.noreply.github.com>
2023-09-30 21:32:47 +0100

    Added comment for proper understanding

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ let Array2 = [5, 6, 3, 10];
 
 let newArray = [];
 
-
+//creating function
 function divideArray (){
+    //giving the condition of equal length
     if(Array1.length != Array2.length){
         console.log("The array length must be of equal length");
     }else{
+        //carrying out the division
+        //let loop through the first array
         for(let i=0; i<Array1.length; i++){
+            //create a container and do the division inside
             let result =Array1[i] / Array2[i];
             // console.log(result);
+            //push my output inside the empty array
             newArray.push(result);
         }
         return newArray;
